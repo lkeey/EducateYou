cc73d9794c3cf91247845220338b1ba8a1a3fbef
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-01-03 22:21:23 +0300

    set up theme

--- composeApp/src/iosMain/kotlin/dev/lkeeeey/edu/you/di/Modules.ios.kt
+++ composeApp/src/iosMain/kotlin/dev/lkeeeey/edu/you/di/Modules.ios.kt
@@ -1,10 +1,11 @@
 package dev.lkeeeey.edu.di
 
 import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module
     get() = module {
-        single<HttpClientEngine> { OkHttp.create() }
+        single<HttpClientEngine> { Darwin.create() }
     }
