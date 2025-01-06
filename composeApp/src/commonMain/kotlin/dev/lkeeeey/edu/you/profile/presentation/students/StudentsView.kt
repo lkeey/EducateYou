6df19a2e877f1edf36e8065d2a9f452f1edf671a
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-01-06 18:09:31 +0300

    create student card

--- composeApp/src/commonMain/kotlin/dev/lkeeeey/edu/you/profile/presentation/students/StudentsView.kt
+++ composeApp/src/commonMain/kotlin/dev/lkeeeey/edu/you/profile/presentation/students/StudentsView.kt
@@ -1,9 +1,29 @@
 package dev.lkeeeey.edu.you.profile.presentation.students
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.lkeeeey.edu.you.core.presentation.Theme
+import dev.lkeeeey.edu.you.profile.presentation.components.ErrorMessage
 import dev.lkeeeey.edu.you.profile.presentation.students.viewmodel.StudentsAction
 import dev.lkeeeey.edu.you.profile.presentation.students.viewmodel.StudentsEvent
 import dev.lkeeeey.edu.you.profile.presentation.students.viewmodel.StudentsState
+import educateyou.composeapp.generated.resources.Res
+import educateyou.composeapp.generated.resources.Thin
+import org.jetbrains.compose.resources.Font
 
 @Composable
 fun StudentsView (
@@ -11,5 +31,34 @@ fun StudentsView (
     onEvent: (StudentsEvent) -> Unit,
     onOpen: (StudentsAction) -> Unit
 ) {
+    Column (
+        modifier = Modifier
+            .animateContentSize()
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        if (state.errorMessage.isNotEmpty()) {
+            ErrorMessage(state.errorMessage)
+        }
 
+        Text(
+            text = "Мои ученики",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(Res.font.Thin)),
+                fontWeight = FontWeight(600),
+                color = Theme.colors.blackProfile,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        if (state.students.isNotEmpty()) {
+            state.students.forEachIndexed { index, student ->
+
+            }
+        }
+    }
 }
